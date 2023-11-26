035fe5447b05755595d8720d5eccf7ce47ca63df
MorozovPV <MorozovPV@alrosa.ru>
2023-11-26 15:26:40 +0300

    updated test_find_sticker_in_element.py

--- tests/test_find_sticker_in_element.py
+++ tests/test_find_sticker_in_element.py
@@ -2,12 +2,13 @@ from selenium.webdriver.common.by import By
 
 url = 'http://localhost/litecart/en/'
 CARDS = 'ul.listing-wrapper li'
+CARDS1 = '.product'
 STICKER = 'ul.listing-wrapper li .sticker'
 STICKER1 = '.sticker'
 
 def test_stickers(driver):
     driver.get(url)
-    cards = driver.find_elements(By.CSS_SELECTOR, CARDS)
+    cards = driver.find_elements(By.CSS_SELECTOR, CARDS1)
     numer = 1
     for card in cards:
         stickers = card.find_elements(By.CSS_SELECTOR, STICKER1)
