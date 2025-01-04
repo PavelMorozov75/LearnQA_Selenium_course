ac7055ce333f87e851d4f00b284c18dea4e24ac1
MorozovPV <MorozovPV@alrosa.ru>
2025-01-04 17:04:40 +0300

    04.01.25

--- capabilities.py
+++ capabilities.py
@@ -3,7 +3,7 @@ a = [1, 2, 2, 4, 5, 6, 7, 8]
 print(len(a))
 for i in range(0,len(a)):
     a.pop()
-print(a)
+print('a = ', a)
 
 weights = [70, 80, 90]
 costs = [100, 200, 300]
@@ -14,17 +14,29 @@ for weight, cost in zip(weights, costs):
     print(f'Weight: {weight}, Cost: {cost}')
 
 options = webdriver.ChromeOptions()
-# caps = webdriver.DesiredCapabilities.CHROME.copy()
-caps = options.to_capabilities()
+caps = webdriver.DesiredCapabilities.CHROME.copy()
+#caps = options.to_capabilities()
 caps['unexpectedAlertBehaviour'] = "dismiss"
 browser_driver = webdriver.Chrome(desired_capabilities=caps)
 print(browser_driver.capabilities)
 
 browser_driver.quit()
+#_____________________________________________________________
+options = webdriver.ChromeOptions()
+caps = options.to_capabilities()
+caps["profile.default_content_settings.popups"] = 0
+caps["download.default_directory"] = "/path/to/download/directory"
 
+driver = webdriver.Chrome(desired_capabilities=caps)
+print(driver.capabilities)
+driver.quit()
 
 #Новая рабочая схема для selenium выше 4.10
 options = webdriver.ChromeOptions()
 options.set_capability("platformName", "windows")
+options.set_capability('unexpectedAlertBehaviour', 'dismiss')
+
+
 browser_driver = webdriver.Chrome(options=options)
 print(browser_driver.capabilities)
+browser_driver.quit()
